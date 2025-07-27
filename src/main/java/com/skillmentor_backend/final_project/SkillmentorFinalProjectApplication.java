b9a1511cfa29b506fd13f5d3c0e369b9d48d5029
Mohamed-Salman-Afhan <salmanafhan59@gmail.com>
2025-07-27 23:48:28 +0530

    Initial commit

---
+++ src/main/java/com/skillmentor_backend/final_project/SkillmentorFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skillmentor_backend.final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillmentorFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillmentorFinalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skillmentor_backend/final_project/SkillmentorFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillmentor_backend.final_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillmentorFinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
