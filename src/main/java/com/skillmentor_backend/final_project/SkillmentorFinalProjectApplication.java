8c29c59fd453d7188cea9f9fda8ade3f6cadd9a7
Mohamed-Salman-Afhan <salmanafhan59@gmail.com>
2025-08-05 09:45:04 +0530

    excluding SystemMetricsAutoConfiguration.class

--- src/main/java/com/skillmentor_backend/final_project/SkillmentorFinalProjectApplication.java
+++ src/main/java/com/skillmentor_backend/final_project/SkillmentorFinalProjectApplication.java
@@ -2,8 +2,10 @@ package com.skillmentor_backend.final_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// --- IMPORT THIS CLASS ---
+import org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SystemMetricsAutoConfiguration.class })
 public class SkillmentorFinalProjectApplication {
 
 	public static void main(String[] args) {
